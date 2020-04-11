9b33d7cb58591110d5245118d7ab6b1ed15e0bd0
copiedcode <raphael.webdesign@outlook.com>
2020-04-11 18:42:59 +0200

    add http requesting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { StarComponent } from './shared/star.component'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -15,7 +16,9 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
     StarComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, 
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from './product';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+
+    private productURL = 'api/products/products.json'
+    constructor(private http: HttpClient) {
+
+    }
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productURL).pipe(
+            //tap(data => console.log('All: '+ JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse){
+        let errorMessage = '';
+        if(err.error instanceof ErrorEvent){
+            //client side error or network error
+            errorMessage = `Ein Fehler ist aufgetreten: ${err.error.message}`;
+        } else {
+            //backend return unsucessfull
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.log(errorMessage);
+        return throwError(errorMessage);
+    }
+
+}
