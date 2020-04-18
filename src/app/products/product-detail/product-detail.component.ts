67e48ed534f4ea9799dce299a811f0f96932355a
copiedcode <raphael.webdesign@outlook.com>
2020-04-18 23:18:31 +0200

    add shared module

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IProduct } from './product';
+import { IProduct } from '../product';
 
 @Component({
   templateUrl: './product-detail.component.html',
