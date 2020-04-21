a0017617e1f0ec15aa51ed3d35ddc4aad6fc5600
copiedcode <raphael.webdesign@outlook.com>
2020-04-21 19:55:45 +0200

    create page for product details

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductDetailComponent } from './product-detail.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './product-detail/product-detail.guard';
+import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 
